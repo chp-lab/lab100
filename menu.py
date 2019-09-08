06902818246c737086dae3dd21249a3a70799264
chp-lab <ch.ee.psu@outlook.com>
2019-09-08 17:59:34 +0700

    pm checked

--- menu.py
+++ menu.py
@@ -1,8 +1,10 @@
 hungry = raw_input("Are you hungry")
 if hungry=="y":
-print("eat samasa")
-print("eat pizza")
-print("eat burger")
+    print("eat samasa")
+    print("eat pizza")
+    print("eat burger")
 else:
-print("do your homework")
-print("go to sleeping")
+    print("do your homework")
+    print("go to sleeping")
+
+print("passed")
